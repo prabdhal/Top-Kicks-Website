210bf3f417daefc53cafe871e903a57029d54426
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-01-09 01:25:44 -0500

    Update app,js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
